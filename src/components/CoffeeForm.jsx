49a4ef615a96b1771d10407014445b39c5903a84
Nishan Thapa <tnishan110@gmail.com>
2024-10-19 17:32:56 +0545

    Hero section completed

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,7 +1,63 @@
+import {coffeeOptions} from '../utils';
+
 export default function CoffeeForm() {
     return (
         <>
-        
+        <div className="section-header">
+            <i className="fa-solid fa-pencil"></i>
+            <h2>Start Tracking Today!</h2>
+        </div>
+        <h4>Select Coffee Type</h4>
+        <div className="coffee-grid">
+            {coffeeOptions.slice(0,5).map((option, optionIndex) => {
+                return (
+                    <button className='button-card' key={optionIndex}>
+                        <h4>{option.name}</h4>
+                        <p>{option.caffeine}</p> mg
+                    </button>
+                )
+            })}
+            <button className='button-card'>
+                <h4>Others</h4>
+                <p>n/a</p>
+            </button>
+        </div>
+        <select name="coffee-list" id="coffee-list">
+            <option value={null}>Select Type</option>
+            {coffeeOptions.map((option, optionIndex) => {
+                return (
+                    <option key={optionIndex} value={option.name}>{option.name} {option.caffeine}mg</option>
+                )
+            })}
+        </select>
+        <h4>Add your cost ($)</h4>
+        <input type="number" placeholder='4.50' className='w-full' />
+        <h4>Time since consumption</h4>
+            <div className="time-entry">
+                <div>
+                    <h6>Hours</h6>
+                    <select id="hours-select">
+                        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map((hour, hourIndex) => {
+                            return (
+                                <option key={hourIndex} value={hour}>{hour}</option>
+                            )
+                        })}
+                    </select>
+                </div>
+                <div>
+                    <h6>Mins</h6>
+                    <select id="mins-select">
+                        {[0, 5, 10, 15, 30, 45].map((min, minIndex) => {
+                            return (
+                                <option key={minIndex} value={min}>{min}</option>
+                            )
+                        })}
+                    </select>
+                </div>
+                <button>
+                    Add Entry
+                </button>
+            </div>
         </>
     )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,11 +3,22 @@ export default function Layout(props){
   const {children} = props
 
   const header = (
-    <header></header>
+    <header>
+      <div>
+        <h1 className="text-gradient">CAFFIEND</h1>
+        <p>For Coffee Insatiates</p>
+      </div>
+      <button>
+        <p>Sign up for free</p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
   )
 
   const footer = (
-    <footer></footer>
+    <footer>
+      <p><span className="text-gradient">Caffiend</span> was made by <a href="https://github.com/TNishan10" target="_blank">NishanThapa</a></p>
+    </footer>
   )
 
   return (
