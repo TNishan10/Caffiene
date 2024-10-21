c51e7127a1459ef65d59b3d10ed548e4a7ea7a39
Nishan Thapa <tnishan110@gmail.com>
2024-10-21 11:25:58 +0545

    Stats card added

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,7 +3,8 @@ function StatCard(props){
 
     return(
         <div className={"card stat-card " + (lg ? ' col-span-2 ': '')}>
-
+            <h4>{title}</h4>
+            {children}
         </div>
     )
 
@@ -11,6 +12,13 @@ function StatCard(props){
 
 
 export default function Stats() {
+    const stats = {
+        daily_caffiene : 240,
+        daily_cost : 2.40,
+        average_coffee : 2,
+        total_cost : 100
+    }
+    
     return (
         <>
         <div className="section-header">
@@ -19,6 +27,10 @@ export default function Stats() {
         </div>
         <div className="stats-grid">
             <StatCard lg title="Active Caffiene level"></StatCard>
+                <div className="status">
+                    <p><span></span></p>
+                </div>
+            <StatCard title="Total Caffiene"></StatCard>
             <StatCard title="Daily Caffiene"></StatCard>
             <StatCard title="Avg # of Coffees"></StatCard>
             <StatCard title="Daily Cost ($)"></StatCard>
