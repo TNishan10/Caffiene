73eafc2c9324f180790e60d0ea0eca9dd9faf5e3
Nishan Thapa <tnishan110@gmail.com>
2024-10-22 14:45:33 +0545

    Firebase to be added Blueprint code finished

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,19 +1,32 @@
 import {coffeeOptions} from '../utils';
 import { useState } from 'react';
+import Authentication from './Authentication';
 
-export default function CoffeeForm() {
+export default function CoffeeForm(props) {
+    const {isAuthenticated} = props;
     const [selectedCoffee, setSelectedCoffee] = useState(null);
     const [showCoffeeTypes, setShowCoffeeTypes] = useState(false);
     const [coffeeCost, setCoffeeCost] = useState(0);
     const [hour, setHour] = useState(0);
     const [min, setMin] = useState(0);
+    const [showModal, setShowModal] = useState(false);
 
     function handleSubmitForm(){
+        if(!isAuthenticated){
+            setShowModal(true)
+            return
+        }
         console.log(selectedCoffee, coffeeCost, hour, min  )
     }
 
     return (
         <>
+        {showModal && (<Modal
+        handleCloseModal={() => {
+            setShowModal(false)
+        }}>
+            <Authentication />
+        </Modal>)}
         <div className="section-header">
             <i className="fa-solid fa-pencil"></i>
             <h2>Start Tracking Today!</h2>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,12 @@
+import Authentication from "./Authentication"
+import Modal from "./Modal"
+import { useState } from "react"
+
 export default function Layout(props){
 
   const {children} = props
+
+  const [showModal, setShowModal] = useState(false)
 
   const header = (
     <header>
@@ -8,7 +14,9 @@ export default function Layout(props){
         <h1 className="text-gradient">CAFFIEND</h1>
         <p>For Coffee Insatiates</p>
       </div>
-      <button>
+      <button onClick = {() => {
+        setShowModal(true)
+      }}>
         <p>Sign up for free</p>
         <i className="fa-solid fa-mug-hot"></i>
       </button>
@@ -23,6 +31,13 @@ export default function Layout(props){
 
   return (
     <>
+    {showModal && (
+      <Modal 
+      handleCloseModal={() => {
+      setShowModal(false)
+    }}>
+      <Authentication/>
+    </Modal>)}
     {header}
     <main>
       {children}
